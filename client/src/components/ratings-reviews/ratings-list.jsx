e2819103c2717b3c571bfef7f9b98075a497e1e2
Christina Hunter <frijole16@gmail.com>
2023-03-14 17:55:50 -0700

    Start creating ratings & review skeleton

---
+++ client/src/components/ratings-reviews/ratings-list.jsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+const RatingsList = () => {
+  return (
+    <div>
+      <h2>Ratings List</h2>
+      <h4>Total Review Count and Filtering Options</h4>
+      <ul>
+        <li>Ratings Tile</li>
+        <li>Ratings Tile</li>
+        <li>Ratings Tile</li>
+        <li>Ratings Tile</li>
+      </ul>
+      <button>More Reviews</button>
+      <button>Add A Review</button>
+
+    </div>
+
+  );
+};
+
+export default RatingsList;
