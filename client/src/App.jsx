be7d68162f10919aa02da5d05ee91e3c9f6d58e0
Tim <timdobranski@gmail.com>
2023-03-14 20:43:54 -0700

    Added initial state, props for components, and dependencies: react-testing-library, and react-checkmark

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,4 +1,5 @@
 import React from 'react';
+import { useState, useEffect } from 'react';
 import Overview from './components/overview/Overview.jsx';
 import QuestionAnswer from './components/question-answer/QuestionAnswer.jsx';
 import RatingsReviews from './components/ratings-reviews/RatingsReviews.jsx';
@@ -7,12 +8,63 @@ import RelatedItemsComparison from './components/related-items-comparison/Relate
 
 
 const App = () => {
+
+  const [ productID, setProductID ] = useState(1);
+  const [ styleID, setStyleID ] = useState(1);
+  const [ myOutfit, setMyOutfit ] = useState([]);
+
+  useEffect(() => {
+    setMyOutfit(JSON.parse(localStorage.getItem('myOutfit')))
+  }, [])
+//  [
+//   {
+//     product: 1,
+//     style: 1
+//   }
+//  ]
+  // Function to add item to myOutfit localStorage
+  const addToOutfit = (productID, styleID) => {
+    // Get current outfit array
+    const outfit = JSON.parse(localStorage.getItem('myOutfit'));
+    // Add to it
+    outfit.push({
+      product: productID,
+      style: styleID
+    });
+    // OldOutfit is now updated with the new item
+    setMyOutfit(outfit);
+    localStorage.setItem(JSON.stringify(outfit))
+  }
+  // Function to remove item from myOutfit localStorage
+  const removeFromOutfit = (productID, styleID) => {
+    const outfit = JSON.parse(localStorage.getItem('myOutfit'));
+      // Remove item that has style and product ID
+     outfit = outfit.filter((outfit) => (outfit.style !== styleID && outfit.product !== productID))
+     setMyOutfit(outfit);
+     // Add item to localStorage
+    localStorage.setItem(JSON.stringify(outfit))
+  }
+
   return (
     <div>
-      <Overview />
+      <Overview
+        productID={productID}
+        setProductID={setProductID}
+        styleID={styleID}
+        setStyleID={setStyleID}
+        addToOutfit={addToOutfit}
+      />
+      <RelatedItemsComparison
+        productID={productID}
+        setProductID={setProductID}
+        addToOutfit={addToOutfit}
+        removeFromOutfit={removeFromOutfit}
+
+      />
       <QuestionAnswer />
-      <RatingsReviews />
-      <RelatedItemsComparison />
+      <RatingsReviews
+        productID={productID}
+      />
     </div>
   )
 }
