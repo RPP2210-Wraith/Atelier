e5d256304c73abd34c4559ae56c000c6f987c4fe
Mallow <linlam0923@gmail.com>
2023-03-16 16:33:19 -0400

    added product information

--- client/src/components/overview/Overview.jsx
+++ client/src/components/overview/Overview.jsx
@@ -1,26 +1,61 @@
-import React from 'react';
+import React, {useEffect, useState} from 'react';
+import axios from 'axios';
 
-const Overview = () => {
+import AddToCart from './AddToCart.jsx'
+
+const Overview = ({ productID, setProductID, styleID, setStyleID, addToOutfit }) => {
+
+  const [product, setProduct] = useState({});
+  const [styles, setStyles] = useState([]);
+  const [features, setFeatures] = useState([]);
+
+  console.log(product)
+  console.log(styles)
+
+  useEffect(() => {
+
+    productID = 71699; // use productID 71699 as placeholder for testing
+
+    axios({
+      method: 'GET',
+      url: '/overview',
+      params: {productID: productID}
+    })
+    .then((response) => {
+      setProduct(response.data[0]);
+      setStyles(response.data[1].results);
+      setFeatures(response.data[0].features)
+    })
+  }, [])
+
   return (
     <div>
       <h1>Overview</h1>
-      <div style={{display:'flex'}}>
+      <div style={{display:'flex', margin: 100}}>
         <div style={{width:'60%'}}>Image Gallery</div>
-        <div style={{width:'40%'}}>Product Information
-          <div>rating</div>
-          <div>category</div>
-          <div>title</div>
+
+        <div style={{width:'40%'}}>
+          <div style={{marginBottom:20}}>✩✩✩✩✩ Read all [#] reviews</div>
+          <div>{product.category} <h3>{product.name}</h3></div>
+
           <div>price</div>
           <div> STYLE: style name</div>
           <div>style selector</div>
-          <div><select><option>Select Size</option></select> <select><option>Quantity</option></select></div>
-          <div><button>Add to Cart</button> <button>like</button></div>
+
+          <AddToCart />
+
         </div>
       </div>
-      <div style={{display:'flex'}}>
-        <div style={{width:'60%'}}>Slogan / Description</div>
-        <div style={{width:'40%'}}>Features</div>
+
+      <div style={{display:'flex', margin: 100}}>
+        <div style={{width:'60%'}}><h4>{product.slogan}</h4>{product.description}</div>
+        <div style={{width:'40%'}}><h4>Features</h4>
+          {features.map((feature) => {
+            return <div key={feature.feature}>{feature.feature}: {feature.value}</div>
+          })}
+        </div>
       </div>
+
     </div>
   )
 }
---
+++ server/overview.js
@@ -0,0 +1,31 @@
+const axios = require('axios');
+const config = require('../config.js');
+
+exports.getProduct = (req, res) => {
+  const productID = req.query.productID;
+
+  let urls = [
+    `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${productID}`,
+    `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${productID}/styles`,
+    // `https://app-hrsei-api.herokuapp.com/api/fec2/hr-rpp/products/${productID}/reviews/`
+  ]
+
+  const requests = urls.map((url) => {
+    return axios({
+      method: 'GET',
+      url: url,
+      headers: {
+        'User-Agent': 'request',
+        'Authorization': `${config.TOKEN}`
+        }
+    })
+  })
+
+  axios.all(requests)
+    .then((responses) => {
+      const datas = responses.reduce((datas, response) => {
+        return datas.concat(response.data)
+      }, [])
+      res.send(datas)
+    })
+}
