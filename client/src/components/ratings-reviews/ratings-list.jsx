dadb4550bc8178ecf5a8b8a12a66d135240c40cf
Christina Hunter <frijole16@gmail.com>
2023-03-16 19:39:05 -0700

    Add styling css file

--- client/src/components/ratings-reviews/ratings-list.jsx
+++ client/src/components/ratings-reviews/ratings-list.jsx
@@ -1,15 +1,15 @@
 import React from 'react';
+import RatingsTile from './ratings-tile.jsx';
 
 const RatingsList = () => {
+
+  var test = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10];
   return (
     <div>
       <h2>Ratings List</h2>
       <h4>Total Review Count and Filtering Options</h4>
       <ul>
-        <li>Ratings Tile</li>
-        <li>Ratings Tile</li>
-        <li>Ratings Tile</li>
-        <li>Ratings Tile</li>
+        {test.map((num, index) => <RatingsTile num={num} key={index} />)}
       </ul>
       <button>More Reviews</button>
       <button>Add A Review</button>
