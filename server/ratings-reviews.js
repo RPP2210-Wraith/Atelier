c298e7e2a8bc778c1e94ec7287763d807114a702
Christina Hunter <frijole16@gmail.com>
2023-03-18 12:52:59 -0700

    API call skeleton

---
+++ server/ratings-reviews.js
@@ -0,0 +1,10 @@
+const axios = require('axios');
+//import the github auth token
+const auth = require('../config.js');
+
+//grab some data from the API via an axios call
+exports.getReviews = () => {
+  //test call
+  console.log('Request for review data made.');
+};
+
