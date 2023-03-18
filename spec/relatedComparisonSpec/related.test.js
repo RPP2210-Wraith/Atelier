de036e0ce20b22305ea7f791760e0f105712615d
Tim <timdobranski@gmail.com>
2023-03-18 12:19:17 -0700

    Updated test suite with needed dependencies and began writing tests to check for render of expected components

--- spec/relatedComparisonSpec/related.test.js
+++ spec/relatedComparisonSpec/related.test.js
@@ -1,2 +1,14 @@
 import Related from '../../client/src/components/related-items-comparison/Related.jsx';
-import
+import RelatedCard from '../../client/src/components/related-items-comparison/Related.jsx';
+import { render, screen } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import React from 'react';
+
+describe('Related', () => {
+  it('renders', () => {
+    render(<Related />);
+    const related = screen.getByText(/Loading Related Items.../i);
+    expect(related).toBeInTheDocument()
+  })
+});
+
