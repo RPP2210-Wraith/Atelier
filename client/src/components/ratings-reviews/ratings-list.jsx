4c63002d8e76cd3e5668c69ea0be75e83b3de327
Christina Hunter <frijole16@gmail.com>
2023-03-25 14:44:39 -0700

    Sort by relevance button basic css implementation

--- client/src/components/ratings-reviews/ratings-list.jsx
+++ client/src/components/ratings-reviews/ratings-list.jsx
@@ -3,23 +3,40 @@ import RatingsTile from './ratings-tile.jsx';
 
 const RatingsList = (props) => {
 
-  return (
+  const addNewReview = () => {
+    console.log('Add new review clicked!');
+  };
 
+  const seeMoreReviews = () => {
+    console.log('See more reviews clicked!');
+  };
+
+  const sortReviews = () => {
+    console.log('Sort reviews button clicked!');
+  };
+
+  return (
     <div class='flex-child-2' id='ratingsList' >
       <h2>Ratings List</h2>
-      <h1>{props.reviews ? props.reviews.length + ' reviews, sorted by relevance' : 'Loading...'}</h1>
+      {props.reviews ?
+
+        <div>
+          <h1 class='in-line'>{props.reviews.length + ' reviews, sorted by'}</h1>
+          <h1 class='in-line underline'
+            onClick={sortReviews}> relevance </h1>
+        </div>
+        : 'Loading...'}
       <ul>
         {props.reviews ? props.reviews.map((review, index) => <RatingsTile review={review} key={index} />) : 'Loading...'}
       </ul>
-
       <div class='flex-parent'>
-        <button class='button-center flex-child-1'>More Reviews</button>
-        <button class='button-center flex-child-1'>Add A Review</button>
+        <button class='button-center flex-child-1'
+          onClick={seeMoreReviews}
+        >More Reviews</button>
+        <button class='button-center flex-child-1'
+          onClick={addNewReview}
+        >Add A Review</button>
       </div>
-
-
-
-
     </div>
 
   );
