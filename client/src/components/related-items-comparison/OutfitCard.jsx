9c8dc46c6979f24bd93de67a02bdb881e86c6a8a
Tim <timdobranski@gmail.com>
2023-03-30 20:45:07 -0700

    Added a placeholder image for products with no image

--- client/src/components/related-items-comparison/OutfitCard.jsx
+++ client/src/components/related-items-comparison/OutfitCard.jsx
@@ -1,13 +1,14 @@
 import React from 'react';
 import Comparison from './Comparison.jsx';
 import { useState } from 'react';
+import StarRatings from 'react-star-ratings';
 
 
 const OutfitCard = ({ item, remove, }) => {
 
   const removeItem = () => {remove(item.id, item.style)};
   const onSale = (item.salePrice ? true : false);
-
+  console.log('item.rating: ', typeof item.rating)
   return (
     <div className='card outfitCard'>
       <img src={item.image} className='cardThumbs' alt='Product Image'></img>
@@ -15,7 +16,13 @@ const OutfitCard = ({ item, remove, }) => {
       <h3>{item.name}</h3>
       <p className={ onSale? 'redStrikethrough' : '' }>{item.price}</p>
       <p>{item.salePrice}</p>
-      <p>{`Rating: ${item.rating}`}</p>
+      <StarRatings
+        rating={parseFloat(item.rating)}
+        starRatedColor="orange"
+        numberOfStars={5}
+        starDimension="20px"
+        starSpacing="2px"
+/>
 
       <img></img>
       <button onClick={removeItem}>❌</button>
